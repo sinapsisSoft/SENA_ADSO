8157af00a6b956f5cb09176632ad5e699da31fed
Sinapsis Technologies <sinapsis.soft.developer@gmail.com>
2024-02-07 09:08:04 -0500

    new changes

--- Myproject/public/assets/js/main.js
+++ Myproject/public/assets/js/main.js
@@ -1,19 +1,28 @@
- function validateForm(){
+/**
+ * Author:
+ * Date:
+ * Update Date:
+ * Descriptions:
+ * 
+ */
 
-                var objInputEmail=document.getElementById("user_email");
-                var objInputPassword=document.getElementById("user_password");
-                let valueEmail=objInputEmail.value;
-                let valuePassword=objInputPassword.value;
-                let countValue = valueEmail.length;
+/**This funtions is general for validate the form HTML */
+function validateForm() {
+/**This is the variable declarations*/
+    var objInputEmail = document.getElementById("user_email");
+    var objInputPassword = document.getElementById("user_password");
+    let valueEmail = objInputEmail.value;
+    let valuePassword = objInputPassword.value;
+    let countValue = valueEmail.length;
+/***/
+    console.log(countValue);
+    if (countValue == 0) {
+        alert('no hay email o password');
+        console.log(objInputEmail);
+        objInputEmail.style.background = "red";
+    } else {
+        objInputEmail.disabled = true;
+        objInputEmail.style.background = "green";
+    }
 
-                console.log(countValue);
-                if(countValue == 0) {
-                    alert('no hay email o password');
-                    console.log(objInputEmail);
-                    objInputEmail.style.background="red";
-                } else{
-                    objInputEmail.disabled=true;
-                    objInputEmail.style.background="green";
-                }
-
-                }
+}
