6210a2d18ea6ea656952af563d34158028b561a2
Sinapsis Technologies <sinapsis.soft.developer@gmail.com>
2024-02-07 20:35:32 -0500

    nuevo cambio
    funcion de validacion 2

--- Myproject/public/assets/js/main.js
+++ Myproject/public/assets/js/main.js
@@ -9,12 +9,8 @@
 /**This funtions is general for validate the form HTML */
 function validateForm() {
 /**This is the variable declarations*/
-   
-
     var objForm=document.getElementById("form_login");
     var elements=objForm.querySelectorAll("input");
-
-
     for(let i=0;i<elements.length;i++){
         console.log(elements[i].value);
         if(elements[i].value == ""){
@@ -27,3 +23,34 @@ function validateForm() {
     }
 
 }
+/**
+ * Author:Willian Moreno
+ * Date:07/01/2024
+ * Update Date:07/01/2024
+ * Descriptions:
+ * 
+ */
+
+/**This funtions is general for validate the form HTML */
+function getData(id,e) {
+
+        var objForm=document.getElementById(id);
+        var elements=objForm.querySelectorAll("input");
+        var elementsLeng=elements.length;
+
+        for(let i=0;i<elementsLeng;i++){
+            let element=elements[i];
+            if(element.value=="" || element.length==0){
+                alert("Error: Validate Element");
+                element.classList.add('errorInput');
+                e.preventDefault();
+                return false;
+            }else{
+                element.classList.remove('errorInput');
+            }
+        }
+  
+        e.preventDefault();
+        return  false;
+    
+    }
