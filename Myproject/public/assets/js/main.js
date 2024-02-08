81f13370554030b0a9f794f6b2e3fbfc1933723c
Sinapsis Technologies <sinapsis.soft.developer@gmail.com>
2024-02-07 19:39:09 -0500

    Nuevo cambio
    Funcion para validar todos los input

--- Myproject/public/assets/js/main.js
+++ Myproject/public/assets/js/main.js
@@ -1,7 +1,7 @@
 /**
- * Author:
- * Date:
- * Update Date:
+ * Author:Willian Moreno
+ * Date:07/01/2024
+ * Update Date:07/01/2024
  * Descriptions:
  * 
  */
@@ -9,20 +9,21 @@
 /**This funtions is general for validate the form HTML */
 function validateForm() {
 /**This is the variable declarations*/
-    var objInputEmail = document.getElementById("user_email");
-    var objInputPassword = document.getElementById("user_password");
-    let valueEmail = objInputEmail.value;
-    let valuePassword = objInputPassword.value;
-    let countValue = valueEmail.length;
-/***/
-    console.log(countValue);
-    if (countValue == 0) {
-        alert('no hay email o password');
-        console.log(objInputEmail);
-        objInputEmail.style.background = "red";
-    } else {
-        objInputEmail.disabled = true;
-        objInputEmail.style.background = "green";
+   
+
+    var objForm=document.getElementById("form_login");
+    var elements=objForm.querySelectorAll("input");
+
+
+    for(let i=0;i<elements.length;i++){
+        console.log(elements[i].value);
+        if(elements[i].value == ""){
+            alert("Valide los datos ingresados");
+            elements[i].focus();
+        
+            return false;
+        }
+
     }
 
 }
