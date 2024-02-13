c97420d994f7a6c24b36a91e9da85edd69318ad5
Sinapsis Technologies <sinapsis.soft.developer@gmail.com>
2024-02-12 21:04:17 -0500

    New changes - Create API

---
+++ Myproject/app/Config/Config.php
@@ -0,0 +1,29 @@
+<?php
+
+$host = "localhost";
+$user = "root";
+$password = "";
+$myDB = "apiSENA";
+$data = array();
+$mysqli = new mysqli($host, $user, $password, $myDB);
+
+if ($mysqli->connect_error) {
+  echo ("Faild to connect " . $mysqli->connect_error);
+} else {
+  //echo("Connect OK");
+}
+
+//$query = "SELECT US.User_id,US.User_user,US.User_password,US.User_status_id,UST.User_status_name, US.Role_id,ROL.Role_name FROM `user` US
+//INNER JOIN role ROL ON US.Role_id=ROL.Role_id
+//INNER JOIN user_status UST ON US.User_status_id=UST.User_status_id;";
+
+$query = "SELECT * FROM role";
+
+$result = $mysqli->query($query);
+
+while ($row = $result->fetch_assoc()) {
+  $data[] = $row;
+}
+//$result->free_result();
+//$mysqli->close();
+echo('{"User_id":"1","User_user":"sena@gmail.com","User_password":"123456","User_status_id":"1","Role_id":"1"}');
--- Myproject/public/assets/js/main.js
+++ Myproject/public/assets/js/main.js
@@ -5,23 +5,27 @@
  * Descriptions:
  * 
  */
+/**Varibles*/
+const myModal = new bootstrap.Modal(document.getElementById("modalUser"), {});
+var objForm = null;
+var elements = null;
+var elementsLeng = 0
+
+getDataJson();
 
 /**This funtions is general for validate the form HTML */
 function validateForm() {
-/**This is the variable declarations*/
-    var objForm=document.getElementById("form_login");
-    var elements=objForm.querySelectorAll("input");
-    for(let i=0;i<elements.length;i++){
+    /**This is the variable declarations*/
+    objForm = document.getElementById("form_login");
+    elements = objForm.querySelectorAll("input");
+    for (let i = 0; i < elements.length; i++) {
         console.log(elements[i].value);
-        if(elements[i].value == ""){
+        if (elements[i].value == "") {
             alert("Valide los datos ingresados");
             elements[i].focus();
-        
             return false;
         }
-
     }
-
 }
 /**
  * Author:DIEGO CASALLAS
@@ -33,100 +37,131 @@ function validateForm() {
  * @id is identification the form
  * @e is event the form
  */
-function getData(id,e) {
-
-        var objForm=document.getElementById(id);
-        var elements=objForm.querySelectorAll("input");
-        var elementsLeng=elements.length;
-
-        for(let i=0;i<elementsLeng;i++){
-            let element=elements[i];
-            if(element.value=="" || element.length==0){
-                alert("Error: Validate Element");
-                element.classList.add('errorInput');
-                e.preventDefault();
-                return false;
-            }else{
-                element.classList.remove('errorInput');
-            }
+function getData(id, e) {
+    objForm = document.getElementById(id);
+    elements = objForm.querySelectorAll("input");
+    elementsLeng = elements.length;
+    for (let i = 0; i < elementsLeng; i++) {
+        let element = elements[i];
+        if (element.value == "" || element.length == 0) {
+            alert("Error: Validate Element");
+            element.classList.add('errorInput');
+            e.preventDefault();
+            return false;
+        } else {
+            element.classList.remove('errorInput');
         }
-        //getDataJson();
-        e.preventDefault();
-        return  false;
-    
     }
-    //getDataJson();
+    e.preventDefault();
+    return false;
+}
+
 /**
- * Author:DIEGO CASALLAS
+ * Author:
  * Date:08/01/2024
  * Update Date:
- * Descriptions: This functions is for get form object and validate
- * Return:value boolean
+ * Descriptions: This functions is clearData
+ * Return:
  * Parameter: 
  * @id is identification the form
- * @e is event the form
- */
 
+*/
 function clearData(id) {
-
-    var objForm=document.getElementById(id);
-
-    var elements=objForm.querySelectorAll("input");
-    var elementsLeng=elements.length;
-    
-    for(let i=0;i<elementsLeng;i++){
-        let element=elements[i];
+    objForm = document.getElementById(id);
+    elements = objForm.querySelectorAll("input");
+    elementsLeng = elements.length;
+    for (let i = 0; i < elementsLeng; i++) {
+        let element = elements[i];
         element.value = "";
     }
 }
 
+/**
+ * Author:
+ * Date:08/01/2024
+ * Update Date:
+ * Descriptions: This functions is formDisabled
+ * Return:
+ * Parameter: 
+ * @id is identification the form
 
+*/
 function formDisabled(id) {
-
-    var objForm=document.getElementById(id);
-    var elements=objForm.querySelectorAll("input");
-    var elementsLeng=elements.length;
-    
-    for(let i=0;i<elementsLeng;i++){
-        let element=elements[i];
+    objForm = document.getElementById(id);
+    elements = objForm.querySelectorAll("input");
+    elementsLeng = elements.length;
+    for (let i = 0; i < elementsLeng; i++) {
+        let element = elements[i];
         element.disabled = true;
-
     }
 }
 
+/**
+ * Author:
+ * Date:08/01/2024
+ * Update Date:
+ * Descriptions: This functions is formEnable
+ * Return:
+ * Parameter: 
+ * @id is identification the form
+
+*/
 function formEnable(id) {
-
-    var objForm=document.getElementById(id);
-    var elements=objForm.querySelectorAll("input");
-    var elementsLeng=elements.length;
-    
-    for(let i=0;i<elementsLeng;i++){
-        let element=elements[i];
+    objForm = document.getElementById(id);
+    elements = objForm.querySelectorAll("input");
+    elementsLeng = elements.length;
+    for (let i = 0; i < elementsLeng; i++) {
+        let element = elements[i];
         element.disabled = false;
-
     }
 }
 
+/**
+ * Author:
+ * Date:08/01/2024
+ * Update Date:
+ * Descriptions: This functions is formEnableEdit
+ * Return:
+ * Parameter: 
+ * @id is identification the form
 
+*/
 function formEnableEdit(id) {
-
-    var objForm=document.getElementById(id);
-    var elements=objForm.querySelectorAll(".input_disabled");
-    var elementsLeng=elements.length;
-    
-    for(let i=0;i<elementsLeng;i++){
-        let element=elements[i];
+    objForm = document.getElementById(id);
+    elements = objForm.querySelectorAll(".input_disabled");
+    elementsLeng = elements.length;
+    for (let i = 0; i < elementsLeng; i++) {
+        let element = elements[i];
         element.disabled = true;
     }
 }
 
+/**
+ * Author:
+ * Date:08/01/2024
+ * Update Date:
+ * Descriptions: This functions is createUser
+ * Return:
+ * Parameter: 
+ * @id is identification the form
 
-function createUser (id) {
+*/
+function createUser(id) {
     clearData(id);
     formEnable(id);
     showModal();
 }
 
+/**
+ * Author:
+ * Date:08/01/2024
+ * Update Date:
+ * Descriptions: This functions is editUser
+ * Return:
+ * Parameter: 
+ * @id is identification the form
+
+*/
 function editUser(id) {
     clearData(id);
     formEnable(id);
@@ -134,30 +169,62 @@ function editUser(id) {
     showModal();
 }
 
+/**
+ * Author:
+ * Date:08/01/2024
+ * Update Date:
+ * Descriptions: This functions is deleteUser
+ * Return:
+ * Parameter: 
+ * @id is identification the user
+
+*/
 function deleteUser(id) {
-  
-   let getConfirm=window.confirm("Seguro desea Eliminar?");
-   //console.log(getConfirm);
-   if(getConfirm){
-alert("OK DELETE");
-   }else{
-    alert("ERROR DELETE");
-   }
+    let getConfirm = window.confirm("Seguro desea Eliminar?");
+    if (getConfirm) {
+        alert("OK DELETE");
+    } else {
+        alert("ERROR DELETE");
+    }
 }
 
+/**
+ * Author:
+ * Date:08/01/2024
+ * Update Date:
+ * Descriptions: This functions is deleteUser
+ * Return:
+ * Parameter: 
+ * @id is identification the form
+ */
 function viewUser(id) {
     clearData(id);
     formDisabled(id);
     showModal();
 }
 
+/**
+ * Author:
+ * Date:08/01/2024
+ * Update Date:
+ * Descriptions: This functions is showModal
+ * Return:
+ * Parameter: 
+ * @id is identification the form
+ */
 function showModal() {
-    var myModal = new bootstrap.Modal(document.getElementById("modalUser"), {});
     myModal.show();
 }
 
+/**
+ * Author:
+ * Date:08/01/2024
+ * Update Date:
+ * Descriptions: This functions is hiddenModal
+ * Return:
+ * Parameter: 
+ * @id is identification the form
+ */
 function hiddenModal() {
-    var myModal = new bootstrap.Modal(document.getElementById("modalUser"), {});
     myModal.hidden();
 }
-//input_disabled
